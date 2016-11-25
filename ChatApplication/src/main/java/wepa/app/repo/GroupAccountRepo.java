dde9645f08c7e79d1b0a447c159a644670a2ad20
lesktimo <l3skt1m@gmail.com>
2016-11-25 14:26:23 +0200

    repot

---
+++ ChatApplication/src/main/java/wepa/app/repo/GroupAccountRepo.java
@@ -0,0 +1,8 @@
+package wepa.app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wepa.app.domain.GroupAccount;
+
+public interface GroupAccountRepo extends JpaRepository<GroupAccount, Long> {
+
+}
---
+++ ChatApplication/src/main/java/wepa/app/repo/TagRepo.java
@@ -0,0 +1,9 @@
+
+package wepa.app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wepa.app.domain.Tag;
+
+public interface TagRepo extends JpaRepository<Tag, Long>{
+    
+}
