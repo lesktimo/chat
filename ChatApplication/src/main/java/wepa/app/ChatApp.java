80a21e548b12c8176b1d2e133a595dd76c54c00b
lesktimo <l3skt1m@gmail.com>
2016-11-25 13:22:08 +0200

    projektin alustus

---
+++ ChatApplication/src/main/java/wepa/app/ChatApp.java
@@ -0,0 +1,12 @@
+package wepa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ChatApp.class, args);
+    }
+}
