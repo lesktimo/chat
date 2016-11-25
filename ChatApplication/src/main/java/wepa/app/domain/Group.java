913f777cecf3bc770e9f6aa6fd72a395ea623066
Toivonen Kim P <tokito@da3-cs-dk110-07.helsinki.fi>
2016-11-25 14:10:39 +0200

    domain-luokat luotu

---
+++ ChatApplication/src/main/java/wepa/app/domain/Group.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wepa.app.domain;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Group extends AbstractPersistable<Long>{
+    
+    
+    
+    private List<Account> accounts;
+    @OneToMany(mappedBy= "group", fetch = FetchType.EAGER)
+    private List<Message> messages;
+    private List<Tag> tags;
+    
+}
---
+++ ChatApplication/src/main/java/wepa/app/domain/Message.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wepa.app.domain;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Message extends AbstractPersistable<Long>{
+    
+    private Timestamp timestamp;
+    private String content;
+    private Account account;
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    
+    
+    
+}
