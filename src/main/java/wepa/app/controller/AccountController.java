a9cd709ba74d7c2fcae461f540d240c9af8b1c73
Toivonen Kim P <tokito@da3-cs-dk110-02.helsinki.fi>
2016-12-05 17:11:52 +0200

    account.html luotu ja thymeleaf toiminnallisuutta jatkettu

--- src/main/java/wepa/app/controller/AccountController.java
+++ src/main/java/wepa/app/controller/AccountController.java
@@ -1,9 +1,26 @@
 
 package wepa.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import wepa.app.repo.AccountRepo;
 
 @Controller
+@RequestMapping("/accounts")
 public class AccountController {
-    public String getAccount(){return null;}
+    
+    @Autowired
+    private AccountRepo accountRepo;
+    
+    
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public String getAccount(@PathVariable Long id, Model model){
+        model.addAttribute("account", accountRepo.findOne(id));
+        
+                
+        return null;}
 }
