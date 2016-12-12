493d1b83cbd310f1b7ced00d58b6dbfaad49986a
etfairies <triforce@windowslive.com>
2016-12-12 15:42:02 +0200

    syötteiden validointia

--- src/test/java/wepa/app/ui/ChatTest.java
+++ src/test/java/wepa/app/ui/ChatTest.java
@@ -10,10 +10,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("Production")
 public class ChatTest extends FluentTest {
 
     public WebDriver webDriver = new HtmlUnitDriver();
