73f3bf00980c2f293249e05e2778d379a8e17d0a
lesktimo <l3skt1m@gmail.com>
2016-12-15 18:23:36 +0200

    selväsalasana

--- src/main/java/wepa/app/service/AccountService.java
+++ src/main/java/wepa/app/service/AccountService.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import wepa.app.domain.Account;
 import wepa.app.repo.AccountRepo;
@@ -21,7 +22,7 @@ public class AccountService {
 
 
     public void save(Account acc) {
-        acc.setPassword(passwordEncoder().encode(acc.getPassword()));
+//        acc.setPassword(passwordEncoder().encode(acc.getPassword()));
         acc.setRoles(new HashSet<>(roleRepo.findAll()));
         accRepo.save(acc);
     }
@@ -31,7 +32,7 @@ public class AccountService {
     }
 
      @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
