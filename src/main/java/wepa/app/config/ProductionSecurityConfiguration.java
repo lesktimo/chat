548d38550125124b47d845d418bbdb058919329e
lesktimo <l3skt1m@gmail.com>
2016-12-15 17:51:37 +0200

    profiilin nimi typo korjattu

--- src/main/java/wepa/app/config/ProductionSecurityConfiguration.java
+++ src/main/java/wepa/app/config/ProductionSecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Profile("Production")
+@Profile("production")
 @Configuration
 @EnableWebSecurity
 public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapter {
