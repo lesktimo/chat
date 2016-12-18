9a6054afbdd3f60742ddd523f9cf0e8bbcb007e4
etfairies <triforce@windowslive.com>
2016-12-18 23:09:25 +0200

    yritys saada heroku-app toimimaan

--- src/main/java/wepa/app/controller/ChatController.java
+++ src/main/java/wepa/app/controller/ChatController.java
@@ -2,7 +2,7 @@ package wepa.app.controller;
 
 import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
